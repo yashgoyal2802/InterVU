3dab451f3fb156a0d4fc9f4997e572c7f7748335
Naman Garg <naman.garg.naman@gmail.com>
2020-10-30 02:14:22 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ if(process.env.NODE_ENV ==='production'){
   app.use(express.static('client/build'));
 }
 // Routes
-app.get("*",(req, res)=>{
+app.get("/",(req, res)=>{
   res.sendFile(path.resolve(__dirname,"../client","build","index.html"));
 })
 app.use("/api/users", users);
