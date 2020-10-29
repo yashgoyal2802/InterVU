c513290b34b41e9464ca6f0480c38d7f8ba9c1db
Naman Garg <naman.garg.naman@gmail.com>
2020-10-30 02:13:15 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -39,6 +39,9 @@ if(process.env.NODE_ENV ==='production'){
   app.use(express.static('client/build'));
 }
 // Routes
+app.get("*",(req, res)=>{
+  res.sendFile(path.resolve(__dirname,"../client","build","index.html"));
+})
 app.use("/api/users", users);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
