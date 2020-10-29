8aaec4cda4af5cc3d42d34b90ef671358b697c33
Naman Garg <naman.garg.naman@gmail.com>
2020-10-30 02:20:13 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -37,11 +37,12 @@ require("./config/passport")(passport);
 
 if(process.env.NODE_ENV ==='production'){
   app.use(express.static('client/build'));
+  app.get("*",(req, res)=>{
+    res.sendFile(path.resolve(__dirname,"../client","build","index.html"));
+  })
+  
 }
 // Routes
-app.get("/",(req, res)=>{
-  res.sendFile(path.resolve(__dirname,"../client","build","index.html"));
-})
 app.use("/api/users", users);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
