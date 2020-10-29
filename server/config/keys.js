33c7e4c8d6f61645b58d11c69da79868753e1c62
Naman Garg <42968194+SuperBaniya@users.noreply.github.com>
2020-10-30 01:24:57 +0530

    Delete keys.js

--- server/config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb://Yashg:yash11@ds259806.mlab.com:59806/tut-login",
-  secretOrKey: "secret",
-  mongoURI2: "mongodb://enji:nrg321nrg@ds127736.mlab.com:27736/companies",
-};
